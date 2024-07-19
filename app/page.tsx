febf49245a07d04369a2aa215178359de12a1076
jonny-mitts <128611721+jonny-mitts@users.noreply.github.com>
2024-07-19 10:55:44 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <>
+      <div>
+        <h1>This is a test</h1>
+        <img src="https://placehold.co/600x400" alt="logo" />
+      </div>
+    </>
+  );
 }
