63a301bab5ca6bf652a2b2bbd4f6b968ca0f4cde
jonny-mitts <128611721+jonny-mitts@users.noreply.github.com>
2024-10-03 20:51:23 -0400

    changed the report only to always on

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
+import { headers } from "next/headers";
+import Script from "next/script";
+
 export default function RootLayout({ children }) {
+  const nonce = headers().get("x-nonce");
   return (
-    <html>
-      <head />
+    <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://assets.pinterest.com/js/pinit.js"
+          type="text/javascript"
+          nonce={nonce}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server'
- 
+import { NextRequest, NextResponse } from "next/server";
+
 export function middleware(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
+    script-src 'self' https://assets.pinterest.com 'nonce-${nonce}';
+    script-src-elem 'self' https://assets.pinterest.com 'nonce-${nonce}';
+    style-src 'self' https://assets.pinterest.com 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
@@ -13,29 +14,34 @@ export function middleware(request: NextRequest) {
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
-`
+`;
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, ' ')
-    .trim()
- 
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-nonce', nonce)
- 
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+
+  // requestHeaders.set(
+  //   "Content-Security-Policy-Report-Only",
+  //   contentSecurityPolicyHeaderValue
+  // );
+
   requestHeaders.set(
-    'Content-Security-Policy-Report-Only',
+    "Content-Security-Policy",
     contentSecurityPolicyHeaderValue
-  )
- 
+  );
+
   const response = NextResponse.next({
     request: {
       headers: requestHeaders,
     },
-  })
+  });
   response.headers.set(
-    'Content-Security-Policy-Report-Only',
+    "Content-Security-Policy-Report-Only",
     contentSecurityPolicyHeaderValue
-  )
- 
-  return response
+  );
+
+  return response;
 }
